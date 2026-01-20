f5f19a71a37320eeb541eb37596f2a49d80b09fe
Adrian Callies <adrianm.callies@gmail.com>
2026-01-20 11:14:49 -0800

    fix: information api bug

--- src/routes/information.ts
+++ src/routes/information.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import { getInformation } from '../controllers/informationController.js';
-import { contactValidation } from '../middleware/validator.js';
-import { contactRateLimiter } from '../middleware/rateLimiter.js';
 
 const router = express.Router();
 
